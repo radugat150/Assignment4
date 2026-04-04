a58e0dea2bfc3067be98d88229a11c8622548e20
radugat150 <radugat150@gmail.com>
2026-04-05 02:01:08 +0300

    Commit 3: finish code

--- task1.py
+++ task1.py
@@ -4,4 +4,6 @@ def evaluate_polynomial(poly_dict, x):
     for key, value in poly_dict.items():
         p=p+value*(x**key)
     return p
-
+my_poly = {0: -10, 2: 3, 4: 1}
+print (evaluate_polynomial(my_poly, 2))
+print (evaluate_polynomial(my_poly, -1.5))
