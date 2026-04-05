972ba9a8352da3679e8ba27b5d49cb46863632c4
radugat150 <radugat150@gmail.com>
2026-04-05 03:55:03 +0300

    Commit 2: add def analyze_methods

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
 #new file
+def analyze_methods(data):
+    report = {}
+    for item in data:
+        method = item["method"]
+        error = item ["error"]
+        time_ms=item["time_ms"]
+        if report.get(method, default)==default:
+            report["method"]={max_error: error, total_time_ms: time_ms, iterations_count: 1}
+        else:
+            if error>report[method]["error"]:
+                report[method]["max_error"] = error
+            report[method]["total_time_ms"]+=time_ms
+            report[method]["iterations_count"]+=1
+    return report
